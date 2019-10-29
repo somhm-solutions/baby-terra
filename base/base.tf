f19bed50c872d3e4dbf93c10642ed90c28a5518c
somhm-solutions <omhm@omhm.org>
2019-10-29 00:00:43 -0700

    AWS-cli and initial terraform setup

---
+++ base/base.tf
@@ -0,0 +1,14 @@
+/* Base Config */
+
+provider "aws"{
+    access_key = ""
+    secret_key = ""
+    region     = "us-east-2"
+}
+
+resource "aws_instance" "base" {
+    ami = "ami-00c03f7f7f2ec15c3"
+    instance_type = "t2.micro"
+}
+
+
